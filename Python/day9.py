d0fb2e3bad2079b286b301a102bfdc613b7bd001
IgorSchittekat <igor.schittekat@gmail.com>
2021-12-09 11:12:54 +0100

    Day 9: python

---
+++ Python/day9.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+
+def read_input():
+    with open('input9.txt', 'r') as file:
+        lines = file.readlines()
+        lines = [[int(char) for char in line.rstrip()] for line in lines]
+        return np.array(lines)
+
+
+def find_lowpoint(i, j, lines):
+    num = lines[i][j]
+    if i > 0 and num >= lines[i - 1][j]:
+        return False
+    if i + 1 < len(lines) and num >= lines[i + 1][j]:
+        return False
+    if j > 0 and num >= lines[i][j - 1]:
+        return False
+    if j + 1 < len(lines[i]) and num >= lines[i][j + 1]:
+        return False
+    return True
+
+
+def part1():
+    lines = read_input()
+    total = 0
+    for i, line in enumerate(lines):
+        for j, num in enumerate(line):
+            if find_lowpoint(i, j, lines):
+                total += num + 1
+    return total
+
+
+def get_basin_size(i, j, lines, visited, size):
+    if (i, j) in visited or lines[i][j] == 9:
+        return visited, size
+    visited.append((i, j))
+    size += 1
+    if i > 0:
+        visited, size = get_basin_size(i - 1, j, lines, visited, size)
+    if i + 1 < len(lines):
+        visited, size = get_basin_size(i + 1, j, lines, visited, size)
+    if j > 0:
+        visited, size = get_basin_size(i, j - 1, lines, visited, size)
+    if j + 1 < len(lines[i]):
+        visited, size = get_basin_size(i, j + 1, lines, visited, size)
+    return visited, size
+
+
+def part2():
+    lines = read_input()
+    basins = []
+    for i, line in enumerate(lines):
+        for j, num in enumerate(line):
+            if find_lowpoint(i, j, lines):
+                _, size = get_basin_size(i, j, lines, [], 0)
+                basins.append(size)
+    return np.prod(sorted(basins)[-3:])
+
+
+if __name__ == '__main__':
+    print(part1())
+    print(part2())
